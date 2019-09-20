bacde3248f93677ff2d513eb0d0097221d6aff08
Jake Daryl Moresca <JakeDaryl.Moresca@infor.com>
2019-09-20 09:31:29 +0800

    Updates

---
+++ precache-manifest.6df3215cbd993d7b140eeaeb511371d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "654bc734083cc54431e2a02c6cc69fd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f538c971bb52a67e1914",
+    "url": "/static/css/2.0f7a873c.chunk.css"
+  },
+  {
+    "revision": "29b890e052004f806810",
+    "url": "/static/css/main.d8e05aca.chunk.css"
+  },
+  {
+    "revision": "f538c971bb52a67e1914",
+    "url": "/static/js/2.aeffd177.chunk.js"
+  },
+  {
+    "revision": "29b890e052004f806810",
+    "url": "/static/js/main.56d1035c.chunk.js"
+  },
+  {
+    "revision": "760bd9c616cb27f54a55",
+    "url": "/static/js/runtime~main.f6a1f042.js"
+  }
+]);
