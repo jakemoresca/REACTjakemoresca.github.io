a9caa70ef8eeac5ac6204fb8e1fc4b1aacdac7e2
jmoresca <jmoresca@outlook.com>
2022-05-16 00:07:04 +0200

    fix build

--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,44 +1,44 @@
 ﻿import * as React from "react";
 import { IExperience } from "./data/experience";
 import { loadExperiences } from "./api/experiences";
-import { InferGetServerSidePropsType } from "next/types";
+import { GetStaticProps, InferGetServerSidePropsType } from "next/types";
 
-interface IExperienceProps
-{
+interface IExperienceProps {
     props: IExperience
 }
 
-export default function Experience(props: InferGetServerSidePropsType<typeof getStaticProps>) {
-    return (
-        <section className="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
-            <div className="w-100">
-                <h2 className="mb-5">Experience</h2>
-                {
-                    props.workExperience.map((x, index) => {
-                        return (
-                            <div key={index} className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                                <div className="resume-content">
-                                    <h3 className="mb-0">{x.position}</h3>
-                                    <div className="subheading mb-3">{x.position}</div>
-                                    <ul>
-                                        {x.responsibilities.map((y, index) => {
-                                            return (<li key={index}>{y}</li>);
-                                        })}
-                                    </ul>
-                                </div>
-                                <div className="resume-date text-md-right">
-                                    <span className="text-primary">October 2020 - Present</span>
-                                </div>
-                            </div>);
-                    })
-                }
-            </div>
+//const Experience: NextFunctionComponent<Props, InitialProps, Context> = ({ posts, subreddit }) => (
+const Experience = ({ props }: IExperienceProps) => (
+    <section className="resume-section p-3 p-lg-5 d-flex justify-content-center" id="experience">
+        <div className="w-100">
+            <h2 className="mb-5">Experience</h2>
+            {
+                props.workExperience.map((x, index) => {
+                    return (
+                        <div key={index} className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+                            <div className="resume-content">
+                                <h3 className="mb-0">{x.position}</h3>
+                                <div className="subheading mb-3">{x.position}</div>
+                                <ul>
+                                    {x.responsibilities.map((y, index) => {
+                                        return (<li key={index}>{y}</li>);
+                                    })}
+                                </ul>
+                            </div>
+                            <div className="resume-date text-md-right">
+                                <span className="text-primary">October 2020 - Present</span>
+                            </div>
+                        </div>);
+                })
+            }
+        </div>
 
-        </section>
-        );
-}
+    </section>
+);
 
-export async function getStaticProps(context): Promise<IExperienceProps> {
+export const getStaticProps: GetStaticProps = async (context) => {
     const response = await loadExperiences();
     return { props: response }
 }
+
+export default Experience;
