22600b85cb0c6d3cfcc4316a9a767a40a5fa2373
rydersir <jake.moresca@outlook.com>
2023-10-15 21:51:39 +0200

    remove details on the page

--- pages/About.tsx
+++ pages/About.tsx
@@ -7,15 +7,14 @@ const About: React.FunctionComponent = () =>
     return (
         <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
             <div className="w-100">
-                <h1 className="mb-0">Jake Daryl
-                    <span className="text-primary"> Moresca</span>
+                <h1 className="mb-0">Page Under 
+                    <span className="text-primary"> Renovation</span>
                 </h1>
                 <div className="subheading mb-5">
-                    +34 610 968 531
-                    <a href="mailto:jmoresca@outlook.com"> jmoresca@outlook.com</a>
+
                 </div>
                 <p className="lead mb-5">
-                    Senior Software Engineer with more than 10 years of industry experience on delivering high quality of work with acceptable timeline. My proficiency on C# and .NET Framework makes me committed on improving my work and knowledge on software development.
+                    
                 </p>
                 <div className="social-icons">
                     <a href="https://www.linkedin.com/in/jake-daryl-moresca/">
--- pages/components/NavMenu.tsx
+++ pages/components/NavMenu.tsx
@@ -20,38 +20,38 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
                 <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" light id="sideNav">
                     <NavbarBrand to="/">
                         <span className="d-block d-lg-none">Jake Daryl Moresca</span>
-                        <span className="d-none d-lg-block">
-                            <Image className="img-fluid img-profile rounded-circle mx-auto mb-2" src={"/img/profile.jpg"} width="1399" height="2048" alt="Profile" />
-                        </span>
+                        {/*<span className="d-none d-lg-block">*/}
+                        {/*    <Image className="img-fluid img-profile rounded-circle mx-auto mb-2" src={"/img/profile.jpg"} width="1399" height="2048" alt="Profile" />*/}
+                        {/*</span>*/}
                     </NavbarBrand>
                     <NavbarToggler onClick={this.toggle} className="navbar-toggler" />
                     <Collapse className="collapse navbar-collapse" isOpen={this.state.isOpen} navbar>
                         <ul className="navbar-nav">
-                            <NavItem>
-                                <Link className="nav-link js-scroll-trigger" href="/" passHref>
-                                    <NavLink className="nav-link js-scroll-trigger">About</NavLink>
-                                </Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="nav-link js-scroll-trigger" href="/experience" passHref>
-                                    <NavLink className="nav-link js-scroll-trigger">Experience</NavLink>
-                                </Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="nav-link js-scroll-trigger" href="/education" passHref>
-                                    <NavLink className="nav-link js-scroll-trigger">Education / Trainings</NavLink>
-                                </Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="nav-link js-scroll-trigger" href="/skills" passHref>
-                                    <NavLink className="nav-link js-scroll-trigger">Skills</NavLink>
-                                </Link>
-                            </NavItem>
-                            <NavItem>
-                                <Link className="nav-link js-scroll-trigger" href="/projects" passHref>
-                                    <NavLink className="nav-link js-scroll-trigger" to="/projects">Personal Projects</NavLink>
-                                </Link>
-                            </NavItem>
+                            {/*<NavItem>*/}
+                            {/*    <Link className="nav-link js-scroll-trigger" href="/" passHref>*/}
+                            {/*        <NavLink className="nav-link js-scroll-trigger">About</NavLink>*/}
+                            {/*    </Link>*/}
+                            {/*</NavItem>*/}
+                            {/*<NavItem>*/}
+                            {/*    <Link className="nav-link js-scroll-trigger" href="/experience" passHref>*/}
+                            {/*        <NavLink className="nav-link js-scroll-trigger">Experience</NavLink>*/}
+                            {/*    </Link>*/}
+                            {/*</NavItem>*/}
+                            {/*<NavItem>*/}
+                            {/*    <Link className="nav-link js-scroll-trigger" href="/education" passHref>*/}
+                            {/*        <NavLink className="nav-link js-scroll-trigger">Education / Trainings</NavLink>*/}
+                            {/*    </Link>*/}
+                            {/*</NavItem>*/}
+                            {/*<NavItem>*/}
+                            {/*    <Link className="nav-link js-scroll-trigger" href="/skills" passHref>*/}
+                            {/*        <NavLink className="nav-link js-scroll-trigger">Skills</NavLink>*/}
+                            {/*    </Link>*/}
+                            {/*</NavItem>*/}
+                            {/*<NavItem>*/}
+                            {/*    <Link className="nav-link js-scroll-trigger" href="/projects" passHref>*/}
+                            {/*        <NavLink className="nav-link js-scroll-trigger" to="/projects">Personal Projects</NavLink>*/}
+                            {/*    </Link>*/}
+                            {/*</NavItem>*/}
                         </ul>
                     </Collapse>
                 </Navbar>
